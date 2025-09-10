50115f2dbfdf7f7e4431810d07b1112120efce24
Lucas-Aguiar-Nunes <lucas_aguiar_nunes@outlook.com>
2025-09-10 19:09:56 -0300

    tela main e rascunho telas portifolios

---
+++ app/src/main/java/com/example/aplicativoap1/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.aplicativoap1
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.aplicativoap1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnErick.setOnClickListener {
+            val intent = Intent(this, tela_erick::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnJulia.setOnClickListener {
+            val intent = Intent(this, tela_julia::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnLucas.setOnClickListener {
+            val intent = Intent(this, tela_lucas::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnMurillo.setOnClickListener {
+            val intent = Intent(this, tela_murillo::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnVinicius.setOnClickListener {
+            val intent = Intent(this, tela_vinicius::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/aplicativoap1/tela_erick.kt
@@ -0,0 +1,30 @@
+package com.example.aplicativoap1
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.aplicativoap1.databinding.ActivityTelaErickBinding
+import androidx.core.net.toUri
+
+class tela_erick : AppCompatActivity() {
+    private lateinit var binding: ActivityTelaErickBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityTelaErickBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.logoGithub.setOnClickListener {
+            val url = "https://github.com/ErickXr"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = url.toUri()
+            startActivity(intent)
+        }
+
+        binding.btnVoltar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
